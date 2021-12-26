4600947382ba46760dd6ef071dfed5a705461fc7
novicejin <gkwlsdnjs95@gmail.com>
2021-12-26 13:18:10 +0900

    spring class6

--- src/t_tok03/ItemTarget.java
+++ src/t_tok03/ItemTarget.java
@@ -2,5 +2,12 @@ package t_tok03;
 
 // 실제 비즈니스 로직을 처리하는 클래스
 public class ItemTarget {
-
+	public void selectItem() {
+		System.out.println("아이템을 조회합니다.");
+	}
+	
+	// 아이템 구매
+	public void buyItem() {
+		System.out.println("아이템을 구매합니다.");
+	}
 }
